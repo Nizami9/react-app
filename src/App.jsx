ed5a3e64488f139dfba308a91235201e3c629d78
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2025-08-20 16:23:51 +0200

    made chnages in App.jsx, adding some test props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Post from "./components/Post.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<main>
+			<Post author="Nizami" body="React.js is awesome"/>
+			<Post author="Fakhri" body="Vue.js is awesome"/>
+			<Post author="Alan" body="Angular .js is awesome"/>
+		</main>
+	);
 }
 
 export default App
